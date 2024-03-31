04d3786a1f2972081feedc499e5a61176af5f251
RobotsBuildingEducation <robotsbuildingeducation@gmail.com>
2024-03-31 00:23:26 -0700

    cleanup store

--- app/src/Store.ts
+++ app/src/Store.ts
@@ -6,12 +6,4 @@ export const useStore = create((set) => ({
   showZap: false,
   setShowStars: () => set((state) => ({ showStars: !state.showStars })),
   setShowZap: () => set((state) => ({ showZap: !state.showZap })),
-
-  // handles background loading for openAI modals
-  // isFetching: false,
-  // response: null,
-  // error: null,
-  // startFetching: () => set(() => ({ isFetching: true })),
-  // setResponse: (response) => set(() => ({ isFetching: false, response })),
-  // setError: (error) => set(() => ({ isFetching: false, error })),
 }));
