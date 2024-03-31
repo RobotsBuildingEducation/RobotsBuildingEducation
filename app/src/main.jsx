ec6a1641500d93ffece884cecf25cd192b47bb86
RobotsBuildingEducation <robotsbuildingeducation@gmail.com>
2024-03-31 16:44:22 -0700

    fucked up idk

---
+++ app/src/main.jsx
@@ -0,0 +1,30 @@
+import ReactDOM from "react-dom/client";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+import App from "./App";
+import DecisionMap from "./experimental/DecisionMap";
+
+// This file handles things before the app launches with route "/".
+// It defines frontend routes and defines a legacy passcode that used to be used to enter the app.
+
+//to visit the experimental component, visit https://robotsbuildingeducation.com/experimental/map
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/experimental/map",
+    element: <DecisionMap />,
+  },
+]);
+
+localStorage.setItem("patreonPasscode", import.meta.env.VITE_PATREON_PASSCODE);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <RouterProvider router={router} />
+);
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VitePWA(), react()],
+  plugins: [react()],
   build: {
     target: "esnext",
   },
@@ -29,10 +28,5 @@ export default defineConfig({
       os: "os-browserify",
     },
     extensions: [".js", ".mjs"],
-  },
-  server: {
-    headers: {
-      "Content-Type": "application/javascript",
-    },
   },
 });
