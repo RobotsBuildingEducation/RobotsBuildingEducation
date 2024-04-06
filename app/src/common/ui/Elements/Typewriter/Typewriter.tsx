c6083e7fe2e84eaa13ebe965e539faeb42b38496
RobotsBuildingEducation <robotsbuildingeducation@gmail.com>
2024-04-06 01:23:39 -0700

    store

--- app/src/common/ui/Elements/Typewriter/Typewriter.tsx
+++ app/src/common/ui/Elements/Typewriter/Typewriter.tsx
@@ -15,7 +15,7 @@ export const processChildren = (children) => {
   return elements;
 };
 
-export const Typewriter = ({ speed = 15, children }) => {
+export const Typewriter = ({ speed = 5, children }) => {
   const [displayedContent, setDisplayedContent] = useState([]);
   const [index, setIndex] = useState(0);
 
